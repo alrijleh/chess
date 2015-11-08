128c8daa0cde90bdc2ee4b82f55bca20335b1e6d
Fouad Al-Rijleh <alrijleh@gmail.com>
2015-11-08 16:31:13 -0500

    populated chess board

--- engine.py
+++ engine.py
@@ -4,6 +4,26 @@
 from board import Board
 from pieces import King, Queen, Rook, Bishop, Knight, Pawn
 
-#test
+#setup board
 board = Board()
-queen = Queen(board,2,2,'white')
+for x in range(8) :
+    Pawn(board, x, 1, 'white')
+    Pawn(board, x, 6, 'black')
+
+for color in ['white', 'black']:
+    if   color == 'white': y = 0
+    elif color == 'black': y = 7
+
+    Rook(board, 0, y, color)
+    Rook(board, 7, y, color)
+
+    Knight(board, 1, y, color)
+    Knight(board, 6, y, color)
+
+    Bishop(board, 2, y, color)
+    Bishop(board, 5, y, color)
+
+    Queen(board, 3, y, color)
+    King(board, 4, y, color)
+
+
