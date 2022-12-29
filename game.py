75556582ad5bbb463c3eb92fcadf05619a116a6a
alrijleh <alrijleh@gmail.com>
2022-12-29 17:39:31 -0500

    added captures, turn counter to display

--- game.py
+++ game.py
@@ -7,25 +7,21 @@ from board import Board
 from pieces import King, Queen, Rook, Bishop, Knight, Pawn
 
 from bots.randombot.randombot import randombot
+from utils import other_color
 
 
 if __name__ == "__main__":
     board = Board()
     board.setup()
-
     print(board)
-    while True:
-        if board.in_checkmate("white") or board.in_stalemate("white"):
-            # cleanup code post game
-            exit(0)
 
-        move = randombot(board, "white")
+    color = 'white'
+    while not board.in_checkmate(color) and not board.in_stalemate(color):
+        if color == 'white':
+            move = randombot(board, color)
+        elif color == 'black':
+            move = randombot(board, color)
         board.play_move(move)
         print(board)
-
-        if board.in_checkmate("black") or board.in_stalemate("black"):
-            # clean up code post game
-            exit(0)
-        move = randombot(board, "black")
-        board.play_move(move)
-        print(board)
+        #input()
+        color = other_color(color)
