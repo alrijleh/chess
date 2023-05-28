d9496e457f812f41a9655da241dcbfb6ca854714
jason <jasonrbreen@gmail.com>
2023-05-28 19:53:34 -0400

    date_bot and usability of game.py

--- game.py
+++ game.py
@@ -2,14 +2,26 @@
 
 from board import Board
 from utils import other_color
+import argparse
 
 #bots
 from bots.randombot.randombot import *
 from bots.bot_fouad import fouad_bot
 from simpbot import simpbot
+from bots.date_bot.date_bot import date_bot
+
+bot_list = {
+    'simpbot': simpbot,
+    'fouad_bot': fouad_bot,
+    'randombot': randombot,
+    'date_bot': date_bot
+}
 
 
-def run_game():
+def run_game(white: str, black: str):
+
+    white_bot = bot_list[white]
+    black_bot = bot_list[black]
     board = Board()
     board.setup()
     print(board)
@@ -17,9 +29,9 @@ def run_game():
     color = "white"
     while not board.in_checkmate(color) and not board.in_stalemate(color):
         if color == "white":
-            move = simpbot(board, color)
+            move = white_bot(board, color)
         elif color == "black":
-            move = fouad_bot(board, color)
+            move = black_bot(board, color)
         move.color = color
         board.play_move(move, color)
         #input()
@@ -32,12 +44,40 @@ def run_game():
         print("you both suck")
         return None
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Get the bots!')
+    parser.add_argument('-white_bot_name', '-w', type=str, help='the name of the bot to use for white')
+    parser.add_argument('-black_bot_name', '-b', type=str, help='the name of the bot to use for black')
+    parser.add_argument('-number_of_games', '-n', type=int, help='the number of games to play', default=10)
+    args = parser.parse_args()
+    bot_choices = list(bot_list.keys())
+    if args.white_bot_name in bot_list.keys() and args.black_bot_name in bot_choices:
+        white_bot_name = args.white_bot_name
+        black_bot_name = args.black_bot_name
+    else:
+        choices = 'Your bot choices are:\nPlease choose a bot for white and black\n'
+        for i in range(len(bot_choices)):
+            choices += f'{i}: {bot_choices[i]}\n'
+        print(choices)
+        white_bot_choice = int(input('Number of white bot choice:\n'))
+        black_bot_choice = int(input('Number of black bot choice:\n'))
+
+        white_bot_name = bot_choices[white_bot_choice]
+        black_bot_name = bot_choices[black_bot_choice]
+
+    return {
+        'white_bot_name': white_bot_name,
+        'black_bot_name': black_bot_name,
+        'number_of_games': args.number_of_games
+    }
 
 if __name__ == "__main__":
+    input_args = get_args()
+
     x = 0
     bookie = {"black": 0, "white": 0, None: 0}
-    while x < 10:
-        winner = run_game()
+    while x < input_args['number_of_games']:
+        winner = run_game(white=input_args['white_bot_name'], black=input_args['black_bot_name'])
         bookie[winner] += 1
         x += 1
 
